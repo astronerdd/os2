c9d85313b5e4b3547666023d70d727f4dada0f5b
DHURANDHAR6969 <gopinathchowdary13@gmail.com>
2026-04-21 12:31:17 +0530

    os

--- priority.c
+++ priority.c
@@ -4,6 +4,8 @@ int main() {
     int n, i, j;
     int bt[10], pr[10], wt[10], tat[10];
 
+    float total_wt = 0, total_tat = 0;
+
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
@@ -41,8 +43,13 @@ int main() {
 
     printf("\nProcess\tBT\tPriority\tWT\tTAT\n");
     for(i = 0; i < n; i++) {
-        printf("P%d\t%d\t%d\t\t%d\t%d\n", i, bt[i], pr[i], wt[i], tat[i]);
+        printf("P%d\t%d\t%d\t\t%d\t%d\n", i+1, bt[i], pr[i], wt[i], tat[i]);
+        total_wt += wt[i];
+        total_tat += tat[i];
     }
+
+    printf("\nAverage Waiting Time = %.2f", total_wt / n);
+    printf("\nAverage Turnaround Time = %.2f\n", total_tat / n);
 
     return 0;
 }
